30cdacdf27cd8bb4e874a078b89505889d0772f7
Tomás del Pino <tomasdelpino28@gmail.com>
2026-07-21 12:00:29 +0200

    Candle aggregator skeleton

---
+++ services/candle-aggregator/src/main/java/com/tickerflow/candle/CandleAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.tickerflow.candle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandleAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandleAggregatorApplication.class, args);
+	}
+
+}
---
+++ services/candle-aggregator/src/test/java/com/tickerflow/candle/CandleAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickerflow.candle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandleAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
